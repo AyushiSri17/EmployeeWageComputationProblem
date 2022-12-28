8168d59bab1f90da7c4ca3fb8c50dfecaa58cd62
“AyushiSri17” <srivastavaayushi7110@gmail.com>
2022-12-28 20:50:03 +0530

    Refector: Solving using Case Statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,17 +2,21 @@ echo "Welcome to Employee Wage Computation Program"
 isPresent=$((RANDOM%3));
 perHourSalary=20;
 workingHour=0;
-if [ $isPresent -eq 0 ]
-then
+case $isPresent in
+	0)
 	echo "Employee is absent";
 	workingHour=0;
-elif [ $isPresent -eq 1 ]
-then
+	;;
+
+	1)
 	echo "Employee is present";
 	workingHour=8;
-else
+	;;
+
+	2)
 	echo "Employee is working as part time";
 	workingHour=4;
-fi
+	;;
+esac
 salary=$(($perHourSalary * $workingHour));
-echo "Employee has earned $salary $ today"
+echo "Employee has earned $salary $ today";
