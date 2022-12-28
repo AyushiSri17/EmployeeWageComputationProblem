06313ceb652cf7979387c916fbca839879680e98
“AyushiSri17” <srivastavaayushi7110@gmail.com>
2022-12-28 21:22:04 +0530

    Added: Calculate Wages Per Month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,22 +1,29 @@
 echo "Welcome to Employee Wage Computation Program"
-isPresent=$((RANDOM%3));
 perHourSalary=20;
 workingHour=0;
-case $isPresent in
-	0)
-	echo "Employee is absent";
-	workingHour=0;
-	;;
+totalSalary=0;
 
-	1)
-	echo "Employee is present";
-	workingHour=8;
-	;;
+for ((day=1; day<=20 ;day++))
+do
+	isPresent=$((RANDOM%3));
+	case $isPresent in
+		0)
+		#echo "Employee is absent";
+		workingHour=0;
+		;;
 
-	2)
-	echo "Employee is working as part time";
-	workingHour=4;
-	;;
-esac
-salary=$(($perHourSalary * $workingHour));
-echo "Employee has earned $salary $ today";
+		1)
+		#echo "Employee is present";
+		workingHour=8;
+		;;
+
+		2)
+		#echo "Employee is working as part time";
+		workingHour=4;
+		;;
+	esac
+	salary=$(($perHourSalary * $workingHour));
+	totalSalary=$(($totalSalary + $salary));
+done
+
+echo "Employee has earned $totalSalary $ in a month";
