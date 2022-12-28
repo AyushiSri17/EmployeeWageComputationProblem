dd2a64fc6b808ae412bb638ced86b25b0bd2f62f
“AyushiSri17” <srivastavaayushi7110@gmail.com>
2022-12-28 22:59:28 +0530

    Refector: Refactor the Code to write a function to get work hours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,35 +1,35 @@
 echo "Welcome to Employee Wage Computation Program"
+function calculateWorkingHour() {
+	case $1 in
+		0)
+			workingHour=0;
+			;;
+
+		1)
+			workingHour=8;
+			;;
+		2)
+			workingHour=4;
+			;;
+	esac;
+	echo $workingHour;
+}
+
 perHourSalary=20;
-workingHour=0;
 totalSalary=0;
 totalWorkingHour=0;
 day=1;
+
 while [[ $day -le 20 && $totalWorkingHour -lt 40 ]]
 do
-	isPresent=$((RANDOM%3));
-	case $isPresent in
-		0)
-		#echo "Employee is absent";
-		workingHour=0;
-		;;
-
-		1)
-		#echo "Employee is present";
-		workingHour=8;
-		;;
-
-		2)
-		#echo "Employee is working as part time";
-		workingHour=4;
-		;;
-	esac
-	totalWorkingHour=$(($totalWorkingHour + $workingHour));
+	wHour=$(calculateWorkingHour $((RANDOM%3)));
+	totalWorkingHour=$(($totalWorkingHour + $wHour));
 	if [ $totalWorkingHour -gt 40 ]
 	then
-		totalWorkingHour=$(($totalWorkingHour - $workingHour));
+		totalWorkingHour=$(($totalWorkingHour - $wHour));
 		break;
 	fi
-	salary=$(($perHourSalary * $workingHour));
+	salary=$(($perHourSalary * $wHour));
 	totalSalary=$(($totalSalary + $salary));
 	((day++));
 done
